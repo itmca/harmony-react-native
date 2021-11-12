8ada7738ee89e6d9e0ffc01f10b178b336cad687
Funncy <gywns41@naver.com>
2021-11-12 22:30:45 +0900

    Add : android settings

--- android/app/src/main/java/com/harmony_app/MainApplication.java
+++ android/app/src/main/java/com/harmony_app/MainApplication.java
@@ -3,6 +3,7 @@ package com.harmony_app;
 import android.app.Application;
 import android.content.Context;
 
+import com.reactnativecommunity.cameraroll.CameraRollPackage;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactInstanceManager;
@@ -29,7 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
+          packages.add(new CameraRollPackage());
           return packages;
         }
 
