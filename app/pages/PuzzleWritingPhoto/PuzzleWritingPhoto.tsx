8f217f5918bdebdd46e9f2e37dee56654696831b
Funncy <gywns41@naver.com>
2021-12-11 17:22:11 +0900

    안드로이드 권한 확인 코드 구현 : Alert 코드 미구현

--- app/pages/PuzzleWritingPhoto/PuzzleWritingPhoto.tsx
+++ app/pages/PuzzleWritingPhoto/PuzzleWritingPhoto.tsx
@@ -1,7 +1,13 @@
 import CameraRoll, {PhotoIdentifier} from '@react-native-community/cameraroll';
 import React, {useEffect, useState} from 'react';
 
-import {Dimensions, ScrollView, View} from 'react-native';
+import {
+  Dimensions,
+  PermissionsAndroid,
+  Platform,
+  ScrollView,
+  View,
+} from 'react-native';
 import {useRecoilValue, useSetRecoilState} from 'recoil';
 
 import SelectablePhoto from '../../components/PuzzleWirtingPhoto/SelectablePhoto';
@@ -23,12 +29,29 @@ const PuzzleWritingPhoto = (): JSX.Element => {
   }, []);
 
   const initPhotos = async () => {
+    if (Platform.OS === 'android' && !(await hasAndroidPermission())) {
+      //TODO: Android 권한 없는 경우 Alert 필요
+      return;
+    }
+
     const result = await CameraRoll.getPhotos({
       first: 20,
       assetType: 'Photos',
     });
     setPhotos(result.edges);
   };
+
+  async function hasAndroidPermission() {
+    const permission = PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE;
+
+    const hasPermission = await PermissionsAndroid.check(permission);
+    if (hasPermission) {
+      return true;
+    }
+
+    const status = await PermissionsAndroid.request(permission);
+    return status === 'granted';
+  }
 
   return (
     <View
