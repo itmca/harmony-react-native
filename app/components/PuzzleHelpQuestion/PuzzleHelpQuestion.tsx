1439f96876780823e368589490e7966cb0f9ab2a
오솔미 <78997066+solmioh@users.noreply.github.com>
2022-03-12 20:56:28 +0900

    PuzzleHelpQuestion (#14)
    * PuzzleHelpQuestion
    
    * PuzzleHelpQuestion 닫기 버튼 아이콘으로 변경
    
    Co-authored-by: solmioh <solshine_@hanmail.net>

---
+++ app/components/PuzzleHelpQuestion/PuzzleHelpQuestion.tsx
@@ -0,0 +1,38 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
+import React, { useState } from 'react';
+import {styles} from './styles';
+import { Image, Text, TouchableOpacity, View } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+
+const PuzzleHelpQuestion = (): JSX.Element => {
+	const [open, setOpen] = useState(true);
+	const onToggle = () => setOpen(!open);
+    
+	return (
+		<View style={styles.container}>
+			<View style={open ? styles.div:styles.smallDiv}>
+				<TouchableOpacity onPress={() => {!open ? setOpen((open) => !open):undefined;}}>
+					<Image 
+						source={require('../../assets/images/puzzle_blue_piece.png')} 
+						style={open ? styles.puzzle:{width: 24, height: 24}} 
+					/>
+				</TouchableOpacity>
+				{open ?
+					<Text style={styles.question}>
+                    초등학교 시절 친구들과 있었던 일 중 가장 기억에 남는 이야기를 들려주세요.
+					</Text> : null}
+				<TouchableOpacity onPress={onToggle}>
+					{open ? 
+						<Icon 
+							name='chevron-left' size={24} 
+							style={styles.close}
+						/> : null
+					}
+				</TouchableOpacity>
+			</View>
+		</View>
+	);
+};
+
+export default PuzzleHelpQuestion;
---
+++ app/components/PuzzleHelpQuestion/styles.tsx
@@ -0,0 +1,68 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#ffffff',
+		paddingHorizontal: 16,
+	},
+	div: {
+		position: 'relative',
+		height: 73,
+		backgroundColor: '#ffffff',
+		borderRadius: 15,
+		shadowColor: '#000',
+		shadowOffset: {
+			width: 2,
+			height: 2,
+		},
+		shadowOpacity: 0.15,
+		elevation: 10,
+		marginTop: 3,
+		paddingLeft: 15,
+		paddingRight: 36.15,
+		flexDirection: 'row',
+		alignItems: 'center',
+	},
+	smallDiv: {
+		width: 32,
+		height: 32,
+		backgroundColor: '#ffffff',
+		borderRadius: 15,
+		shadowColor: '#000',
+		shadowOffset: {
+			width: 2,
+			height: 2,
+		},
+		shadowOpacity: 0.15,
+		elevation: 10,
+		marginTop: 3,
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	puzzle: {
+		width: 33.94,
+		height: 33.25
+	},
+	question: {
+		marginLeft: 24.5,
+		marginRight: 36.15,
+		fontWeight: 'bold',
+		color: '#707070',
+		fontSize: 14,
+		lineHeight: 22,
+		letterSpacing: 0.15
+	},
+	close: {
+		position: 'absolute',
+		right: 0,
+		width: 24.5,
+		height: 24
+	},
+	box: {
+		width: '100%',
+		height: 48,
+		backgroundColor: '#93B0D0',
+		opacity: 0.2
+	}
+});
