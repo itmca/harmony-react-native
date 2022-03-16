92d7ac04f2c3ddef58a3fa96dd17dbc1beba0591
ironprayer <ironprayer1208@gmail.com>
2022-03-16 22:16:29 +0900

    녹음 기능 테스트 - 녹음 중지가 안되는 오류

--- app/pages/PuzzleWritingVoice/PuzzleWritingVoice.tsx
+++ app/pages/PuzzleWritingVoice/PuzzleWritingVoice.tsx
@@ -1,41 +1,57 @@
 import Button from '@ant-design/react-native/lib/button';
-import React from 'react';
-import {recordState} from '../../recoils/RecordRecoil';
-import {useRecoilState} from 'recoil';
+import React, {useState} from 'react';
 
-import {Text, View} from 'react-native';
+import {Image, Text, View} from 'react-native';
 import AudioRecorderPlayer, {
   AVEncoderAudioQualityIOSType,
   AVEncodingOption,
   AudioEncoderAndroidType,
-  AudioSet,
   AudioSourceAndroidType,
+  AVModeIOSOption,
 } from 'react-native-audio-recorder-player';
 
 const PuzzleWritingVoice = (): JSX.Element => {
-  const [_, setRecordState] = useRecoilState(recordState);
+  const [state, setState] = useState({recordSecs: 0, recordTime: '00:00:00'});
   const audioRecorderPlayer = new AudioRecorderPlayer();
-  async function onStartRecord() {
-    const path = 'hello.m4a';
+
+  const onStartRecord = async function () {
+    const path = 'sound.m4a';
     const audioSet = {
       AudioEncoderAndroid: AudioEncoderAndroidType.AAC,
       AudioSourceAndroid: AudioSourceAndroidType.MIC,
+      AVModeIOS: AVModeIOSOption.measurement,
       AVEncoderAudioQualityKeyIOS: AVEncoderAudioQualityIOSType.high,
       AVNumberOfChannelsKeyIOS: 2,
       AVFormatIDKeyIOS: AVEncodingOption.aac,
     };
-    console.log('audioSet', audioSet);
+    // const meteringEnabled = false;
+
+    //console.log('audioSet', audioSet);
     await audioRecorderPlayer.startRecorder(path, audioSet);
-    console.log('end url');
+    //console.log('end url');
     audioRecorderPlayer.addRecordBackListener(e => {
-      setRecordState({
+      setState({
         recordSecs: e.currentPosition,
         recordTime: audioRecorderPlayer.mmssss(Math.floor(e.currentPosition)),
       });
     });
     // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
     //console.log(`uri: ${uri}`);
-  }
+  };
+
+  const onStopRecord = async function () {
+    //console.log('RecordSec', state.recordSecs);
+    //console.log('RecordTime', state.recordTime);
+    // stopRecorder 정상작동하지 않아 멈추지 않은거지. // 파일이 안 늘어야 돼. -> (파일이 계속 늘어남) -> 라이브러리 문제일수도 있다.
+    const result = await audioRecorderPlayer.stopRecorder();
+    // removeRecordBackListener 작동 안했다고 해도.
+    audioRecorderPlayer.removeRecordBackListener();
+    setState({...state, recordSecs: 0});
+    //console.log('RecordSec1', state.recordSecs);
+    //console.log('RecordTime2', state.recordTime);
+    console.log(result);
+  };
+
   return (
     <View
       style={{
@@ -43,9 +59,26 @@ const PuzzleWritingVoice = (): JSX.Element => {
         justifyContent: 'center',
         alignItems: 'center',
       }}>
-      <Text>Puzzle Voice </Text>
+      <Text>{state.recordTime}</Text>
+      <View style={{flexDirection: 'row'}}>
+        <Image source={require('../../assets/images/voice_frequency.png')} />
+        <Image source={require('../../assets/images/voice_frequency.png')} />
+        <Image source={require('../../assets/images/voice_frequency.png')} />
+        <Image source={require('../../assets/images/voice_frequency.png')} />
+      </View>
+      <View>
+        <Button
+          style={{
+            width: 44,
+            height: 44,
+            borderRadius: 44 / 2,
+            borderColor: 'black',
+            margin: 10,
+            backgroundColor: 'red',
+          }}></Button>
+      </View>
       <Button onPress={onStartRecord}>Start</Button>
-      <Button>stop</Button>
+      <Button onPress={onStopRecord}>stop</Button>
     </View>
   );
 };
--- app/recoils/RecordRecoil.ts
+++ app/recoils/RecordRecoil.ts
@@ -2,5 +2,15 @@ import {atom} from 'recoil';
 
 export const recordState = atom({
   key: 'recordState',
-  default: {recordSecs: 0, recordTime: ''},
+  default: {recordSecs: 0, recordTime: '00:00:00'},
+});
+
+export const recordPlayState = atom({
+  key: 'recordPlayState',
+  default: {
+    currentPositionSec: 0,
+    currentDurationSec: 0,
+    playTime: '',
+    duration: '',
+  },
 });
