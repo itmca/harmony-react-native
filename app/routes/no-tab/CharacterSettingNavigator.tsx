25d445977c5ebfe8af36da1ddf7e6f6d0b44d533
ironprayer <ironprayer1208@gmail.com>
2022-07-23 12:42:51 +0900

    profile image upload

--- app/routes/no-tab/CharacterSettingNavigator.tsx
+++ app/routes/no-tab/CharacterSettingNavigator.tsx
@@ -4,6 +4,8 @@ import HeroSetting from '../../pages/HeroSetting/HeroSetting';
 import GoBackHeaderLeft from '../../components/header/GoBackHeaderLeft';
 import HeroRegister from '../../pages/HeroRegister/HeroRegister';
 import HeroModification from '../../pages/HeroModification/HeroModification';
+import HeroSelectingPhoto from '../../pages/HeroSelectingPhoto/HeroSelectingPhoto';
+import WritingHeaderRight from '../../components/header/WritingHeaderRight';
 
 const Stack = createNativeStackNavigator();
 
@@ -34,6 +36,22 @@ const CharacterSettingNavigator = (): JSX.Element => {
         options={{
           headerLeft: () => <GoBackHeaderLeft />,
           title: '주인공 정보 수정',
+        }}
+      />
+      <Stack.Screen
+        name="CharacterSelectingPhoto"
+        component={HeroSelectingPhoto}
+        options={{
+          headerLeft: () => <GoBackHeaderLeft iconType="chevron-left" />,
+          title: '주인공 사진 선택',
+          headerRight: () => (
+            <WritingHeaderRight
+              text="완료"
+              customAction={() => {
+                console.log('test');
+              }}
+            />
+          ),
         }}
       />
     </Stack.Navigator>
